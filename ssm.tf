38e7c7ca9069315c54de5bf8e618edb78a5e261c
Phillip Langley <philliplangley@champmbplangley.lan>
2024-05-13 13:09:46 -0500

    updating module

--- ssm.tf
+++ ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "this" {
-  name        = "/${var.git}/s3-iam-access-key/${aws_iam_access_key.this.id}/id"
+  name        = "/${var.git}-${var.name}/s3-iam-access-key/${aws_iam_access_key.this.id}/id"
   description = "s3 iam access key id"
   type        = "SecureString"
   value       = aws_iam_access_key.this.secret
