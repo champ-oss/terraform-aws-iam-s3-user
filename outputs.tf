cf3812701a45f94a112586617b149b612a684aac
Phillip Langley <philliplangley@champmbplangley.lan>
2024-05-13 12:50:36 -0500

    resolve linting issue

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
 output "s3_access_key_id" {
-  value = aws_iam_access_key.this.id
+  value       = aws_iam_access_key.this.id
+  description = "The access key ID for the S3 bucket"
 }
 
 output "s3_secret_access_key" {
-  value     = aws_iam_access_key.this.secret
-  sensitive = true
+  value       = aws_iam_access_key.this.secret
+  description = "The secret access key for the S3 bucket"
+  sensitive   = true
 }
